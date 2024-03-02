5b344a66aeeb5e1f3e4aaccb2b7c9fc3b46e0e15
Aashish <128910552+Aashish-18@users.noreply.github.com>
2024-03-02 16:19:03 +0530

    KeyListener.java

--- src/KeyListener.java
+++ src/KeyListener.java
@@ -1,29 +1,29 @@
-//import javax.swing.*;
-//import java.awt.event.*;
-//
-//public class KeyListener implements KeyListener {
-//    JFrame f;
-//    JLabel l;
-//    JTextArea t;
-//
-//    KeyListener(){
-//        f=new JFrame("KeyListener");
-//        l=new JLabel();
-//        l.setBounds(10,20,120,30);
-//        t=new JTextArea();
-//        t.setBounds(30,50,200,130);
-//        t.addKeyListener(this);
-//        f.add(l);
-//        f.add(t);
-//
-//        f.setSize(400,400);
-//        f.setLayout(null);
-//        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//        f.setResizable(false);
-//        f.setVisible(true);
-//
-//    }
-//    public static void main(String args[]){
-//        new KeyListener();
-//    }
-//}
+import javax.swing.*;
+import java.awt.event.*;
+
+public class KeyListener implements KeyListener {
+   JFrame f;
+   JLabel l;
+   JTextArea t;
+
+   KeyListener(){
+       f=new JFrame("KeyListener");
+       l=new JLabel();
+       l.setBounds(10,20,120,30);
+       t=new JTextArea();
+       t.setBounds(30,50,200,130);
+       t.addKeyListener(this);
+       f.add(l);
+       f.add(t);
+
+       f.setSize(400,400);
+       f.setLayout(null);
+       f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+       f.setResizable(false);
+       f.setVisible(true);
+
+   }
+   public static void main(String args[]){
+       new KeyListener();
+   }
+}
